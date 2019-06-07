f526092516d87029091bae909f4a5889e9a259c8
Yang Hu <hyytez@gmail.com>
2019-06-07 16:01:35 +0800

    update

--- localusers/luser-demo02.sh
+++ localusers/luser-demo02.sh
@@ -11,3 +11,9 @@ USER_NAME=$(id -un)
 echo "Your username is ${USER_NAME}"
 
 # Display if the user is the root user or not
+if [[ "${UID}" = 0 ]]
+then
+    echo 'You are root'
+else
+    echo 'You are not root'
+fi
